5547ebf02d9a82740c3d9154cbdac88c28d63faf
GitHubTest20222 <109940656+GitHubTest20222@users.noreply.github.com>
2022-08-03 14:16:42 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,12 +15,12 @@ install_3proxy() {
     echo "installing 3proxy"
     URL="https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy
+    cd 3proxy-0.9.4
     make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    cp src/3proxy /usr/local/etc/3proxy/bin/
-    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-    chmod +x /etc/init.d/3proxy
+    mkdir -p /usr/local/etc/3proxy-0.9.4/{bin,logs,stat}
+    cp src/3proxy /usr/local/etc/3proxy-0.9.4/bin/
+    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy-0.9.4
+    chmod +x /etc/init.d/3proxy-0.9.4
     chkconfig 3proxy on
     cd $WORKDIR
 }
